cfa5b7c697c3c1e80f2612a3ab928bcd35885703
Vlad <ciorescuvladstefan0@gmail.com>
2025-10-29 15:05:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-linear-to-br from-green-900 to-blue-900 text-white text-4xl font-bold">
+      Tailwind + Typescript + Vite✅
+      
+    </div>
+  );
+}
