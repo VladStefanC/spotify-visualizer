c933ba9dfd31115a82039d584c322fd6849f41f7
Vlad <ciorescuvladstefan0@gmail.com>
2025-10-30 12:07:13 +0200

    added login redirect to spotify 0auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
+import { useEffect,useState } from "react";
+import SpotifyLogin from "./components/SpotifyLogin";
+
+
+
 export default function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-linear-to-br from-green-900 to-blue-900 text-white text-4xl font-bold">
-      Tailwind + Typescript + Vite✅
-      
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    const storedToken = window.localStorage.getItem("token");
+
+    if(!storedToken && hash) {
+      const _token = hash.substring(1).split("&").find((param) => param.startsWith("access_token"))?.split("=")[1];
+
+      window.location.hash="";
+
+      if(_token) {
+        window.localStorage.setItem("token", _token);
+        setToken(_token);
+
+      }
+    } else if (storedToken) {
+      setToken(storedToken);
+    }
+
+  }, []);
+
+  const handleLogout = () => {
+    setToken(null);
+    window.localStorage.removeItem("token");
+
+  };
+
+  if(!token){
+    return <SpotifyLogin />;
+
+  }
+
+
+  return(
+    <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-white">
+      <h1 className="text-3xl font-bold mb-4"> 🎧 Logged in to Spotify! </h1>
+      <button onClick={handleLogout}
+      className="bg-red-500 px-6 py-2 rounded-md hover:bg-red-600 transition">
+        Logout
+      </button>
     </div>
-  );
+  )
 }
---
+++ src/spotifyAuth.ts
@@ -0,0 +1,11 @@
+const CLIENT_ID = "3ec1baa5d2e649d28f46d12f54b37cfc"
+const REDIRECT_URI = "https://localhost:5173"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
+
+const SCOPES = [
+    "user-read-currently-playing",
+    "user-read-playback-state"
+];
+
+export const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=${RESPONSE_TYPE}&scope=${SCOPES.join("%20")}`;
