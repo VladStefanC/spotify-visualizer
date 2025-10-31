70b6d97037a65c10a32521ca5648dbc68e5ddb75
Vlad <ciorescuvladstefan0@gmail.com>
2025-10-31 13:10:27 +0200

    test gain.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -141,7 +141,7 @@ export default function App() {
           />
         )}
       </div>
-      <div className="flex object-top-left justify-center min-h-screen bg-neutral-700 text-white gap-6">
+      <div className="flex items-center justify-center min-h-screen bg-neutral-700 text-white gap-6">
         <div className="text-center">
           <h1 className="text-3xl font-bold mb-2">
             {profile?.display_name ?? "Unknown listener"}
