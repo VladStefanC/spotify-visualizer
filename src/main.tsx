0747efdea617771636f58bb162fbb4714f9b2973
Vlad <ciorescuvladstefan0@gmail.com>
2025-11-04 15:14:48 +0200

    BIG CHANGES COMING PART 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import '/src/app/App.css'
+
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
