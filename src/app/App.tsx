6ad13620aaece09ddf5570049d570c3a0ac014ad
Vlad <ciorescuvladstefan0@gmail.com>
2025-11-07 11:11:47 +0200

    Trying to position vynil position to be responive 3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -283,20 +283,12 @@ export default function App() {
               <div className="absolute top-1/2 left-1/2 h-28 w-28 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white" />
               <div className="absolute top-1/2 left-1/2 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black" />
             </div>
-          </div>
 
-          <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-            <div className="relative flex w-[min(90vw,32rem)] max-w-3xl items-center justify-center">
-              <div className="relative aspect-square size-[clamp(12rem,40vw,22rem)] shrink-0">
-                {/* vinyl layers stay the same */}
-              </div>
-
-              <img
+            <img
                 src={nowPlaying.albumImage}
                 alt={nowPlaying.trackName ?? ""}
                 className="relative z-10 ml-[-20%] aspect-square size-[clamp(10rem,36vw,19rem)] rounded-none object-cover shadow-[0_0_20px_rgba(0,0,0,0.35)]"
               />
-            </div>
           </div>
         </>
       )}
